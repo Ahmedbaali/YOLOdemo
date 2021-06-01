7ec4460283ccb0b5e5225a646c6da5777b35c7f2
Ahmed BAALI <baali.ahmed7@gmail.com>
2021-06-01 12:30:55 +0200

    adding a code

--- yoloDemo.py
+++ yoloDemo.py
@@ -1,1 +1,16 @@
+import cv2
+import numpy as np
+
+cap = cv2.VideoCapture(0)
+
+while True:
+    success, img = cap.read()
+
+    cv2.imshow('showme', img)
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+cap.release()
+cv2.destroyAllWindows()
+
 
